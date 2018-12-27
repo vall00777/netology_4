369d3730ff0c721e7bdb747653a4a2abc5e4e6c0
Валерий <36442895+vall00777@users.noreply.github.com>
2018-12-27 09:58:10 +0400

    Update api.py

--- api.py
+++ api.py
@@ -3,8 +3,6 @@
 import json
 import requests
 
-parse_response = lambda response: json.loads(response)["response"]
-parse_error = lambda response: json.loads(response)['error']
 
 MANY_REQUESTS_PER_SECOND_ERROR = 6
 
@@ -20,57 +18,66 @@ class API(object):
     def __init__(self, key):
         self.key = key
 
-    def get_users_ids(self, id):
-        print('- friends.get {}'.format(id))
+    @staticmethod
+    def _parse_response(response):
+        return json.loads(response)["response"]
+
+    @staticmethod
+    def _parse_error(response):
+        return json.loads(response)['error']
+
+    def get_users_ids(self, user_id):
+        print('- friends.get {}'.format(user_id))
         params = list()
-        params.append('user_id=' + str(id))
+        params.append('user_id=' + str(user_id))
         req = self.req_url.format("friends.get", "&".join(params), self.key, self.api_version)
         response = requests.get(req).text
         if 'error' in response:
-            resp = parse_error(response)
+            resp = self._parse_error(response)
             if int(resp['error_code']) == MANY_REQUESTS_PER_SECOND_ERROR:
-                return self.get_user_groups(id)
+                return self.get_user_groups(user_id)
             else:
                 raise ApiException(resp['error_msg'])
 
-        return parse_response(response)['items']
+        return self._parse_response(response)['items']
 
-    def get_user_groups(self, id):
-        print('- groups.get {}'.format(id))
+    def get_user_groups(self, user_id):
+        print('- groups.get {}'.format(user_id))
         params = list()
-        params.append('user_id='+str(id))
+        params.append('user_id=' + str(user_id))
         req = self.req_url.format("groups.get", "&".join(params), self.key, self.api_version)
         response = requests.get(req).text
         if 'error' in response:
-            resp = parse_error(response)
+            resp = self._parse_error(response)
             if int(resp['error_code']) == MANY_REQUESTS_PER_SECOND_ERROR:
-                return self.get_user_groups(id)
+                return self.get_user_groups(user_id)
             else:
                 raise ApiException(resp['error_msg'])
 
-        return parse_response(response)['items']
+        return self._parse_response(response)['items']
 
-    def get_id(self, id):
-        print('- users.get {}'.format(id))
+    def get_id(self, user_id):
+        print('- users.get {}'.format(user_id))
         params = list()
-        params.append('user_ids=' + str(id))
+        params.append('user_ids=' + str(user_id))
         req = self.req_url.format("users.get", "&".join(params), self.key, self.api_version)
         response = requests.get(req).text
-        return parse_response(response)[0]['id']
+        return self._parse_response(response)[0]['id']
 
-    def get_group_info(self, id):
-        print('- groups.getById {}'.format(id))
+    def get_group_info(self, user_id):
+        print('- groups.getById {}'.format(user_id))
         params = list()
-        params.append('group_id=' + str(id))
+        params.append('group_id=' + str(user_id))
         params.append('fields=members_count')
         req = self.req_url.format("groups.getById", "&".join(params), self.key, self.api_version)
         response = requests.get(req).text
         if 'error' in response:
-            resp = parse_error(response)
+            resp = self._parse_error(response)
             if int(resp['error_code']) == MANY_REQUESTS_PER_SECOND_ERROR:
-                return self.get_group_info(id)
+                return self.get_group_info(user_id)
             else:
                 raise ApiException(resp['error_msg'])
 
-        response = parse_response(response)
+        response = self._parse_response(response)
         return response[0]['id'], response[0]['name'], response[0]['members_count']
+
